7f18cffe890b1788d232a5535c5776ee7c4169cd
Hélène Ruelle <ruellehelene26@gmail.com>
2024-12-04 13:39:12 +0100

    add API fetch w/ local state and error/loading/data rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./styles/App.css";
+import CardLayout from "./components/CardLayout";
+import fetchProjects from "./api/projects";
+import projectsViewModel from "./helpers/projectsViewModel";
+
+const DEFAULT_STATE = {
+  data: null,
+  loading: false,
+  error: null,
+};
 
 function App() {
+  const [state, setState] = useState(DEFAULT_STATE);
+
+  useEffect(() => {
+    // Fetch projects
+    const loadData = async () => {
+      try {
+        setState({ ...DEFAULT_STATE, loading: true });
+        const data = await fetchProjects();
+        setState({ ...DEFAULT_STATE, data: projectsViewModel(data) });
+      } catch (error) {
+        console.error("Error fetching cards data:", error);
+        setState({ ...DEFAULT_STATE, error });
+      } finally {
+        setState((prevState) => ({ ...prevState, loading: false }));
+      }
+    };
+
+    loadData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="container">
+        {state.loading && <div>Loading...</div>}
+        {state.data && !state.loading && <CardLayout projects={state.data} />}
+        {state.error && !state.loading && (
+          <div>We encountered an error while retrieving the data.</div>
+        )}
+      </div>
     </div>
   );
 }
